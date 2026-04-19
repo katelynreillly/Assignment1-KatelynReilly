17a95033ad4ec9cd2db725639a864243f6c7b87e
katelynreillly <katelyn.reilly@msj.edu>
2026-04-19 19:32:12 -0400

    Add dark mode toggle functionality

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 window.onload = function() {
     alert("✨ New projects have been added to my boards! Scroll to explore my latest marketing and tech work.");
 };
+const button = document.querySelector("#themeBtn");
+
+button.addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        button.textContent = "Light Mode";
+    } else {
+        button.textContent = "Dark Mode";
+    }
+});
